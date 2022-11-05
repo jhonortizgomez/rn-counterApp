ae7ba65a296a0307fb4aa01742024f8d3f6b68c6
Jhon Gomez <estebanortizgomez96@gmail.com>
2022-11-04 19:34:23 -0500

    feat: create mobile app for count the press to a button

---
+++ src/components/Fab.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import {
+  Text,
+  TouchableOpacity,
+  Platform,
+  View,
+  StyleSheet,
+  TouchableNativeFeedback,
+} from 'react-native';
+
+interface Props {
+  title: string;
+  position?: 'br' | 'bl';
+  onPress: () => void;
+}
+
+export const Fab = ({title, onPress, position = 'br'}: Props) => {
+  const ios = () => {
+    return (
+      <TouchableOpacity
+      onPress={onPress}
+        activeOpacity={ 0.8 }
+        style={[
+          styles.fabLocation,
+          position === 'bl' ? styles.left : styles.right,
+        ]}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>{title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  const android = () => {
+    return (
+      <View
+        style={[
+          styles.fabLocation,
+          position === 'bl' ? styles.left : styles.right,
+        ]}>
+        <TouchableNativeFeedback
+          onPress={onPress}
+          background={TouchableNativeFeedback.Ripple('black', false, 30)}>
+          <View style={styles.fab}>
+            <Text style={styles.fabText}>{title}</Text>
+          </View>
+        </TouchableNativeFeedback>
+      </View>
+    );
+  };
+
+  return Platform.OS === 'ios' ? ios() : android();
+};
+
+const styles = StyleSheet.create({
+  fabLocation: {
+    position: 'absolute',
+    bottom: 25,
+  },
+  right: {
+    right: 25,
+  },
+  left: {
+    left: 25,
+  },
+
+  fab: {
+    backgroundColor: '#5856D6',
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 9,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 12.35,
+
+    elevation: 10,
+  },
+  fabText: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+});
---
+++ src/screens/HolaMundoScreen.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const HolaMundoScreen = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        // backgroundColor: 'red',
+        justifyContent: 'center',
+      }}>
+      <Text
+        style={{
+          fontSize: 45,
+          textAlign: 'center',
+        }}>
+        Hola Jhon
+      </Text>
+    </View>
+  );
+};
