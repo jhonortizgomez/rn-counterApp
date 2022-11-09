4eeddc182602afcfc2df451583e41584b6b417d0
Jhon Gomez <estebanortizgomez96@gmail.com>
2022-11-08 19:01:13 -0500

    feat: create screen for practice css in react native

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {CounterScreen} from './src/screens/CounterScreen';
+import {SafeAreaView} from 'react-native';
+import {TareaScreen} from './src/screens/TareaScreen';
+// import {FlexScreen} from './src/screens/FlexScreen';
+// import {PositionScreen} from './src/screens/PositionScreen';
+// import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen';
+// import {Dimensiones} from './src/screens/Dimensiones';
+// import {CounterScreen} from './src/screens/CounterScreen';
 // import {HolaMundoScreen} from './src/screens/HolaMundoScreen';
 
 export const App = () => {
   return (
-    // <HolaMundoScreen />
-    <CounterScreen />
+    <SafeAreaView style={{flex: 1, backgroundColor: '#28425B'}}>
+      {/* // <HolaMundoScreen />
+      // <CounterScreen /> */}
+      {/* <BoxObjectModelScreen /> */}
+      {/* <Dimensiones /> */}
+      {/* <PositionScreen /> */}
+      {/* <FlexScreen /> */}
+      <TareaScreen />
+    </SafeAreaView>
   );
 };
---
+++ src/screens/PositionScreen.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+export const PositionScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.cajaVerde}></View>
+      <View style={styles.cajaMorada}></View>
+      <View style={styles.cajaNaranja}></View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    // width: 400,
+    // height: 400,
+    backgroundColor: '#28c4D9',
+  },
+  cajaMorada: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#5856D6',
+    borderWidth: 10,
+    borderColor: 'white',
+    position: 'absolute',
+    top: 0,
+    right: 0,
+  },
+  cajaNaranja: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#F0A23B',
+    borderWidth: 10,
+    borderColor: 'white',
+    position: 'absolute',
+    right: 0,
+    bottom: 0,
+  },
+  cajaVerde: {
+    backgroundColor: 'green',
+    borderWidth: 10,
+    borderColor: 'white',
+    ...StyleSheet.absoluteFillObject,
+  },
+});
